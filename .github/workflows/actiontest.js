a426de95be17c04095bfc7ae95e9cd275d582ec4
subratanp <51863679+subratanp@users.noreply.github.com>
2023-12-31 20:54:06 +1100

    Testing Now

--- .github/workflows/actiontest.js
+++ .github/workflows/actiontest.js
@@ -1,10 +1,6 @@
 let core=require('@actions/core');
 
-<<<<<<< HEAD
 var pas="pass";
-=======
-var pas="dfdf"
->>>>>>> 2d68aa9 (Testing Now)
 var testinput=core.getInput('param1',{required:true});
 
 core.info("This is a test input: " +  testinput);
